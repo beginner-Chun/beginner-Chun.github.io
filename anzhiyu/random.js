39caf7fa72132afb759d64bb3ebe1e4aaad70959
beginner-Chun <2917314230@qq.com>
2024-05-08 20:13:41 +0800

    Site updated: 2024-05-08 20:13:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/06/SSM/","2024/05/06/SpringSecurity/","2024/05/06/redis应用篇/","2024/05/08/MybatisPlus/","2024/05/02/归去来兮辞/"];function toRandomPost(){
+var posts=["2024/05/08/MybatisPlus/","2024/05/02/归去来兮辞/","2024/05/06/SSM/","2024/05/06/SpringSecurity/","2024/05/06/redis应用篇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
